ad5e8b01f017ce78d3c2053c2296660b11759579
Steve Goldhaber <stevenng@met.no>
2026-07-29 12:46:40 +0200

    Attempt to fix NetCDF finding

--- machines/container/container.cmake
+++ machines/container/container.cmake
@@ -2,14 +2,11 @@ if (COMP_NAME STREQUAL gptl)
   string(APPEND CPPDEFS " -DHAVE_NANOTIME -DBIT64 -DHAVE_VPRINTF -DHAVE_BACKTRACE -DHAVE_SLASHPROC -DHAVE_COMM_F2C -DHAVE_TIMES -DHAVE_GETTIMEOFDAY")
 endif()
 
-#set(NETCDF_PATH "/opt/netcdf")
-#set(NETCDF_C_PATH "/opt/netcdf")
-#set(NETCDF_FORTRAN_PATH "/opt/netcdf")
+set(NetCDF_C_PATH "$ENV{NETCDF_C_ROOT}")
+set(NetCDF_Fortran_PATH "$ENV{NETCDF_FORTRAN_ROOT}")
 
 # Add our cmake finder scripts
 list (APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/cmake)
-
-list(APPEND NetCDF_Fortran_INCLUDE_DIRS /opt/netcdf/include)
 list(APPEND NetCDF_C_LIBRARIES /opt/netcdf/include)
 
 set(LDFLAGS "")
